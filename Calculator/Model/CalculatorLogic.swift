b4311f653ee2a6b0aef1e81dfae1c30bd4410bf6
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-08 18:50:50 -0600

    314. Guard let vs. If Let When to Use Which?

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -10,26 +10,51 @@ import Foundation
 
 struct CalculatorLogic {
 	
-	var number: Double?
+	private var number: Double?
+	
+	private var intermediateCalculation: (n1: Double, calcMethod: String)?
 	
 	mutating func setNumber(_ number: Double) {
 		self.number = number
 	}
 	
-	func calculate(symbol: String) -> Double? {
+	mutating func calculate(symbol: String) -> Double? {
 		guard let number else {
 			fatalError("number is nil")
 		}
 		switch symbol {
-		case "+/-" : return number * -1
-		case "AC" : return 0
-		case "%" : return number * 0.01
-		case "+" : return 0
-		case "=" : return 0
+		case "+/-" :
+			return number * -1
+		case "AC" :
+			return 0
+		case "%" :
+			return number * 0.01
+		case "=" :
+			return performTwoNumCalculation(n2: number)
 		default:
-			print("not implemented yet")
-			return nil
+			intermediateCalculation = (n1: number, calcMethod: symbol)
 		}
+		return nil
+	}
+	
+	private func performTwoNumCalculation(n2: Double) -> Double? {
+		guard let intermediateCalculation else {
+			fatalError("intermediateCalculation is nil")
+		}
+		let (n1, calcMethod) = intermediateCalculation
+		switch calcMethod {
+		case "+" :
+			return n1 + n2
+		case "-" :
+			return n1 - n2
+		case "×" :
+			return n1 * n2
+		case "÷" :
+			return n1 / n2
+		default:
+			fatalError("The operation \(calcMethod) passed in does not match any of the cases.")
+		}
+		return nil
 	}
 }
 
